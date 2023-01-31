d55d3e515fce615331e673380b8548bba5f2d630
pppfe <pppfe@192.168.1.21>
2023-02-01 00:42:20 +0100

    ver 0.13.1 fix

--- resources/views/Profile/message.blade.php
+++ resources/views/Profile/message.blade.php
@@ -3,13 +3,17 @@
 @section ('content')
 <div class="content">
     @foreach ($myuser->notyetviewed() as $notify)
+        @if ($notify->pivot->id_sender != $myuser->id)
         NOTIFICA DA: {{$notify->name}} {{$notify->surname}}
         <button id='margintop' onclick= "location.href ='{{ route('messagesview',$notify->id)}}'">VISUALIZZA</button>
+        @endif
     @endforeach
     
     @foreach ($myuser->viewed() as $list)
+        @if ($list->pivot->id_sender != $myuser->id)
         NOTIFICA LETTA: {{$list->name}} {{$list->surname}}
         <button id='margintop' onclick= "location.href ='{{ route('messagesview',$list->id)}}'">VISUALIZZA</button>
+        @endif
     @endforeach
 </div>
 @endsection
